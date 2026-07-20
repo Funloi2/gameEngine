0522267108f615f92cfd5921718adb8f70a22435
Charles <ce.marguerite@sudalys.fr>
2026-07-20 13:34:35 +0200

    set up fps counter

--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -1,22 +1,26 @@
 package com.base.engine;
 
-import com.sun.tools.javac.Main;
-import org.lwjgl.opengl.Display;
-
 public class MainComponent {
 
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
     private static final String TITLE = "Game Engine";
 
+    int frames = 0;
+    long frameCounter = 0;
+    final static double FRAME_CAP = 1000.0;
+
     private boolean isRunning;
 
+    private Game game;
     public MainComponent() {
         isRunning=false;
+        game = new Game();
     }
 
     public void start() {
         if(isRunning) return;
+
         run();
     }
 
@@ -27,9 +31,53 @@ public class MainComponent {
 
     private void run() {
         isRunning = true;
+
+        long lastTime = Time.getTime();
+        double unprocTime = 0;
+
+        final double frameTime = 1.0/ FRAME_CAP;
+
+
         while(isRunning){
-            if(Window.isCloseRequested()) stop();
-            render();
+            boolean render = false;
+            long startTime = Time.getTime();
+            long passedTime = startTime - lastTime ;
+            lastTime = startTime;
+
+            unprocTime +=passedTime / (double)Time.SECOND;
+
+            while(unprocTime > frameTime){
+
+                render = true;
+
+                unprocTime -= frameTime;
+                frameCounter += passedTime;
+
+                if(Window.isCloseRequested()) stop();
+
+                Time.setDelta(frameTime);
+
+                game.input();
+                game.update();
+                if(frameCounter >= Time.SECOND){
+
+                    System.out.println(frames);
+                    frames = 0;
+                    frameCounter = 0;
+                }
+            }
+            if (render){
+                render();
+                frames++;
+
+            }
+            else{
+                try{
+                    Thread.sleep(1);
+                }catch (InterruptedException error){
+                    error.printStackTrace();
+                }
+            }
         }
 
         cleanUp();
@@ -37,6 +85,7 @@ public class MainComponent {
 
     private void render() {
         Window.render();
+        game.render();
     }
 
     private void cleanUp() {
